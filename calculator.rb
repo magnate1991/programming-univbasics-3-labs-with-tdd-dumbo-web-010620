87e2f8c1184d979c533d89913ac3b0c716bbc8fe
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2019-12-28 09:05:28 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,5 +3,5 @@ second_number = "2"
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-quorient = first_number 
+quorient = first_number / second_number
 
